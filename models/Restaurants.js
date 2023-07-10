4f1592b27eedd9e3394627680bfa1277edc6ecfe
olamarvel <78472252+olamarvel@users.noreply.github.com>
2023-07-10 23:17:49 +0000

    added images to restrurants

--- models/Restaurants.js
+++ models/Restaurants.js
@@ -7,6 +7,10 @@ const RestaurantsSchema = new Schema({
     type: String,
     required: true
   },
+  image: {
+    type: String,
+    required: true
+  },
   avrating: {
     type: String,
     mininum: 0,
--- validation/restaurants.js
+++ validation/restaurants.js
@@ -4,9 +4,13 @@ const isEmpty = require("./is-empty");
 module.exports = function validateRestaurant(data) {
   let errors = {};
   data.name = !isEmpty(data.name) ? data.name : "";
+  data.name = !isEmpty(data.image) ? data.image : "";
 
   if (Validator.isEmpty(data.name)) {
     errors.name = "Name field is required";
+  }
+  if (Validator.isEmpty(data.image)) {
+    errors.image = "Image field is required";
   }
   return {
     errors,
